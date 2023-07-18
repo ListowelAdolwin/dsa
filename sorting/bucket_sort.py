8a13065fa5da6d9c05dd3a41f5f871d7910ef8f6
ListowelAdolwin <listoweladolwin@gmail.com>
2023-07-18 08:13:19 +0000

    Bucket sort

--- sorting/bucket_sort.py
+++ sorting/bucket_sort.py
@@ -4,9 +4,7 @@
 def bucket_sort(arr):
     count = [0] * (max(arr) + 1)
     for n in arr:
-        print(n)
         count[n] = count[n] + 1
-    print(count)
 
     i = 0
     result = []
@@ -14,6 +12,6 @@ def bucket_sort(arr):
         for _ in range(count[n]):
             arr[i] = n
             i += 1
-arr =[1, 5, 4, 3, 1]
+arr =[1, 5, 40, 3, 1]
 bucket_sort(arr)
 print(arr)
